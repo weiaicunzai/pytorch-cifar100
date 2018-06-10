d4a76aedc6a4bfdb4c7dfab91cf83d2d5a4efa88
weiaicunzai <372995411@qq.com>
2018-06-10 22:35:19 +0800

    resnet complete(without relu)

--- models/resnet.py
+++ models/resnet.py
@@ -4,18 +4,24 @@
 
 
 
-
 [1] Kaiming He, Xiangyu Zhang, Shaoqing Ren, Jian Sun 
     Deep Residual Learning for Image Recognition
     https://arxiv.org/abs/1512.03385
 """
 
-import torch.nn as nn
 import torch
+import torch.nn as nn
 
 
 class BasicBlock(nn.Module):
-    # why this even exsist
+    """Basic Block for resnet 18 and resnet 34
+
+    """
+
+    #BasicBlock and BottleNeck block 
+    #have different output size
+    #we use class attribute expansion
+    #to distinct
     expansion = 1
 
     def __init__(self, in_channels, out_channels, stride=1):
@@ -25,16 +31,18 @@ class BasicBlock(nn.Module):
         self.residual_function = nn.Sequential(
             nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=stride, padding=1, bias=False),
             nn.BatchNorm2d(out_channels),
-            nn.Conv2d(out_channels, out_channels, kernel_size=3, stride=1, padding=1, bias=False),
+            nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1, bias=False),
             nn.BatchNorm2d(out_channels)
         )
 
         #shortcut
         self.shortcut = nn.Sequential()
 
+        #the shortcut output dimension is not the same with residual function
+        #use 1*1 convolution to match the dimension
         if stride != 1 or in_channels != BasicBlock.expansion * out_channels:
             self.shortcut = nn.Sequential(
-                nn.Conv2d(in_channels, out_channels, kernel_size=1, stride=stride, padding=1, bias=False),
+                nn.Conv2d(in_channels, out_channels, kernel_size=1, stride=stride, bias=False),
                 nn.BatchNorm2d(out_channels)
             )
         
@@ -42,23 +50,124 @@ class BasicBlock(nn.Module):
         return self.residual_function(x) + self.shortcut(x)
 
 
+class BottleNeck(nn.Module):
+    """Residual block for resnet over 50 layers
 
+    """
+    expansion = 4
+    def __init__(slef, in_channels, out_channels, stride=1):
+        super().__init__()
+        self.residual_function = nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, kernel_size=1, bias=False),
+            nn.BatchNorm2d(out_channels),
+            nn.Conv2d(out_channels, out_channels, stride=stride, kernel_size=3, padding=1, bias=False),
+            nn.BatchNorm2d(out_channels),
+            nn.Conv2d(out_channels, out_channels * BottleNeck.expansion, kernel_size=1, bias=Fase),
+            nn.BatchNorm2d(out_channels * BottleNeck.expansion)
+        )
+
+        self.shortcut = nnSequential()
+
+        if stride != 1 or in_channels != out_channels * BottleNeck.expansion:
+            self.shortcut = nn.Sequential(
+                nn.Conv2d(in_channels, out_channels * BottleNeck.expansion, kernel_size=1, bias=1),
+                nn.BatchNorm2d(out_channels)
+            )
+        
+    def forward(self, x):
+        return self.residual_function(x) + self.shortcut(x)
+    
 
 class ResNet(nn.Module):
 
     def __init__(self, block, num_block, num_classes=10):
         super().__init__()
 
-        self.out_channels = 64
+        self.in_channels = 64
 
-        self.conv1 = nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1, bias=False)
-        self.bn = nn.BatchNorm2d(64)
+        self.conv1 = nn.Sequential(
+            nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1, bias=False),
+            nn.BatchNorm2d(64))
+        #we use a different inputsize than the original paper
+        #so conv2_x's stride is 1
+        self.conv2_x = self._make_layer(block, 64, num_block[0], 1)
+        self.conv3_x = self._make_layer(block, 128, num_block[1], 2)
+        self.conv4_x = self._make_layer(block, 256, num_block[2], 2)
+        self.conv5_x = self._make_layer(block, 512, num_block[3], 2)
+        self.avg_pool = nn.AvgPool2d(4)
+        self.fc = nn.Linear(512 * block.expansion, num_classes)
+
+
+
+    def _make_layer(self, block, out_channels, num_blocks, stride):
+        """make resnet layers(by layer i didnt mean this 'layer' was the 
+        same as a neuron netowork layer, ex. conv layer), one layer may 
+        contain more than one residual block 
+
+        Args:
+            block: block type, basic block or bottle neck block
+            out_channels: output depth channel number of this layer
+            num_blocks: how many blocks per layer
+            stride: the stride of the first block of this layer
+        
+        Return:
+            return a resnet layer
+        """
+
+        # we have num_block blocks per layer, the first block 
+        # could be 1 or 2, other blocks would always be 1
+        strides = [stride] + [1] * (num_blocks - 1)
+        layers = []
+        for stride in strides:
+            layers.append(block(self.in_channels, out_channels, stride))
+            self.in_channels = out_channels * block.expansion
+        
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        residual = x
+        output = self.conv1(x)
+        output = self.conv2_x(output)
+        output = self.conv3_x(output)
+        output = self.conv4_x(output)
+        output = self.conv5_x(output)
+        output = self.avg_pool(output)
+        output = output.view(output.size(0), -1)
+        output = self.fc(output)
+
+        return output 
+
+
+def ResNet18():
+    """ return a ResNet 18 object
+    """
+    return ResNet(BasicBlock, [2, 2, 2, 2])
+
+def ResNet34():
+    """ return a ResNet 34 object
+    """
+    return ResNet(BasicBlock, [3, 4, 6, 3])
+
+def ResNet50():
+    """ return a ResNet 50 object
+    """
+    return ResNet(BottleNeck, [3, 4, 6, 3])
+
+def ResNet101():
+    """ return a ResNet 101 object
+    """
+    return ResNet(BottleNeck, [3, 4, 23, 3])
+
+def ResNet152():
+    """ return a ResNet 152 object
+    """
+    return ResNet(BottleNeck, [3, 8, 36, 3])
 
 
 
 
-    def _make_layer(self, block, out_channels, stride):
-        stride = [stride] + [1] * (num_block - 1)
+from torch.autograd import Variable
 
-
-
+test = Variable(torch.randn(1, 3, 32, 32))
+resnet18 = ResNet18()
+print(resnet18(test))
