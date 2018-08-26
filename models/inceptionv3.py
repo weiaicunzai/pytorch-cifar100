edce9b943b4beca0d55749b6fef1736ee891f5a3
weiaicunzai <372995411@qq.com>
2018-08-26 22:22:25 +0800

    [save work]

---
+++ models/inceptionv3.py
@@ -0,0 +1,10 @@
+""" inceptionv3 in pytorch
+
+
+[1] Christian Szegedy, Vincent Vanhoucke, Sergey Ioffe, Jonathon Shlens, Zbigniew Wojna
+
+    Rethinking the Inception Architecture for Computer Vision
+    https://arxiv.org/abs/1512.00567v3
+"""
+
+
