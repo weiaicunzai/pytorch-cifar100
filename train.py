14be675e8a1470f4d6bf99bb574a7630bdb6cfaa
weiaicunzai <372995411@qq.com>
2018-11-02 13:43:04 +0800

    Update train.py

--- train.py
+++ train.py
@@ -12,6 +12,7 @@ from datetime import datetime
 
 import numpy as np
 import torch
+import torch.nn as nn
 import torch.optim as optim
 import torchvision
 import torchvision.transforms as transforms
