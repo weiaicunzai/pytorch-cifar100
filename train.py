cbaa7edea03dde682351dc4ee839cc1c510dc52f
weiaicunzai <372995411@qq.com>
2018-11-06 10:37:38 +0800

    [bug fixed] correct -> correct.float()

--- train.py
+++ train.py
@@ -146,7 +146,7 @@ def eval_training(epoch):
     writer.add_scalar('Test/Average loss', test_loss / len(cifar100_test), epoch)
     writer.add_scalar('Test/Accuracy', correct.float() / len(cifar100_test), epoch)
 
-    return correct / len(cifar100_test)
+    return correct.float() / len(cifar100_test)
 
 def main():
     #use tensorboard
