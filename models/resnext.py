02b64cccd27b5bedb2fd2b3e58015a680857b43a
weiaicunzai <372995411@qq.com>
2018-11-13 21:05:10 +0800

    add resnext.py

---
+++ models/resnext.py
@@ -0,0 +1,9 @@
+"""resnext in pytorch
+
+
+
+[1] Saining Xie, Ross Girshick, Piotr Dollár, Zhuowen Tu, Kaiming He.
+
+    Aggregated Residual Transformations for Deep Neural Networks
+    https://arxiv.org/abs/1611.05431
+"""
