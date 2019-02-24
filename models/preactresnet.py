de57b6166b7e165aac58a7c37ccb5c079cb4d2d8
weiaicunzai <372995411@qq.com>
2019-02-24 11:36:12 +0800

    [bug fixed] wrong output channels for preactresnet18 and preactresnt34

--- models/preactresnet.py
+++ models/preactresnet.py
@@ -12,6 +12,7 @@ import torch.nn.functional as F
 
 class PreActBasic(nn.Module):
 
+    expansion = 1
     def __init__(self, in_channels, out_channels, stride):
         super().__init__()
         self.residual = nn.Sequential(
@@ -24,7 +25,7 @@ class PreActBasic(nn.Module):
         )
 
         self.shortcut = nn.Sequential()
-        if stride != 1 or in_channels != out_channels:
+        if stride != 1 or in_channels != out_channels * PreActBasic.expansion:
             self.shortcut = nn.Conv2d(in_channels, out_channels, 1, stride=stride)
         
     def forward(self, x):
@@ -37,28 +38,29 @@ class PreActBasic(nn.Module):
 
 class PreActBottleNeck(nn.Module):
 
+    expansion = 4
     def __init__(self, in_channels, out_channels, stride):
         super().__init__()
 
-        bottleneck_channel = int(out_channels / 4)
+        #bottleneck_channel = int(out_channels / 4)
         self.residual = nn.Sequential(
             nn.BatchNorm2d(in_channels),
             nn.ReLU(inplace=True),
-            nn.Conv2d(in_channels, bottleneck_channel, 1, stride=stride),
+            nn.Conv2d(in_channels, out_channels, 1, stride=stride),
 
-            nn.BatchNorm2d(bottleneck_channel),
+            nn.BatchNorm2d(out_channels),
             nn.ReLU(inplace=True),
-            nn.Conv2d(bottleneck_channel, bottleneck_channel, 3, padding=1),
+            nn.Conv2d(out_channels, out_channels, 3, padding=1),
 
-            nn.BatchNorm2d(bottleneck_channel),
+            nn.BatchNorm2d(out_channels),
             nn.ReLU(inplace=True),
-            nn.Conv2d(bottleneck_channel, out_channels, 1)
+            nn.Conv2d(out_channels, out_channels * PreActBottleNeck.expansion, 1)
         )
 
         self.shortcut = nn.Sequential()
 
-        if stride != 1 or in_channels != out_channels:
-            self.shortcut = nn.Conv2d(in_channels, out_channels, 1, stride=stride)
+        if stride != 1 or in_channels != out_channels * PreActBottleNeck.expansion:
+            self.shortcut = nn.Conv2d(in_channels, out_channels * PreActBottleNeck.expansion, 1, stride=stride)
     
     def forward(self, x):
 
@@ -71,6 +73,7 @@ class PreActResNet(nn.Module):
 
     def __init__(self, block, num_block, class_num=100):
         super().__init__()
+        self.input_channels = 64
 
         self.pre = nn.Sequential(
             nn.Conv2d(3, 64, 3, padding=1),
@@ -78,20 +81,22 @@ class PreActResNet(nn.Module):
             nn.ReLU(inplace=True)
         )
 
-        self.stage1 = self._make_layers(block, num_block[0], 64, 256, 1)
-        self.stage2 = self._make_layers(block, num_block[1], 256, 512, 2)
-        self.stage3 = self._make_layers(block, num_block[2], 512, 1024, 2)
-        self.stage4 = self._make_layers(block, num_block[3], 1024, 2048, 2)
+        self.stage1 = self._make_layers(block, num_block[0], 64,  1)
+        self.stage2 = self._make_layers(block, num_block[1], 128, 2)
+        self.stage3 = self._make_layers(block, num_block[2], 256, 2)
+        self.stage4 = self._make_layers(block, num_block[3], 516, 2)
 
-        self.linear = nn.Linear(2048, class_num)
+        self.linear = nn.Linear(self.input_channels, class_num)
     
-    def _make_layers(self, block, num, in_channels, out_channels, stride):
+    def _make_layers(self, block, block_num, out_channels, stride):
         layers = []
 
-        layers.append(block(in_channels, out_channels, stride))
-        while num - 1:
-            layers.append(block(out_channels, out_channels, 1))
-            num -= 1
+        layers.append(block(self.input_channels, out_channels, stride))
+        self.input_channels = out_channels * block.expansion
+
+        while block_num - 1:
+            layers.append(block(self.input_channels, out_channels, 1))
+            block_num -= 1
         
         return nn.Sequential(*layers)
 
@@ -123,3 +128,4 @@ def preactresnet101():
 
 def preactresnet152():
     return PreActResNet(PreActBottleNeck, [3, 8, 36, 3])
+
