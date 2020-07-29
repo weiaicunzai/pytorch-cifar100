cb2ad2727342f7535b0e741e470a86061bde754b
weiaicunzai <372995411@qq.com>
2020-07-30 01:11:45 +0800

    Fix argparse boolean arguments always set to true error.(#9) Summary: Issue resolved: #9
    Set argument value type to bool would make ```-s False``` and ```-gpu False```
    also be ```True```, use ```action='store_true'``` instead.

--- train.py
+++ train.py
@@ -106,10 +106,10 @@ if __name__ == '__main__':
     
     parser = argparse.ArgumentParser()
     parser.add_argument('-net', type=str, required=True, help='net type')
-    parser.add_argument('-gpu', type=bool, default=True, help='use gpu or not')
+    parser.add_argument('-gpu', action='store_true', default=False, help='use gpu or not')
     parser.add_argument('-w', type=int, default=2, help='number of workers for dataloader')
     parser.add_argument('-b', type=int, default=128, help='batch size for dataloader')
-    parser.add_argument('-s', type=bool, default=True, help='whether shuffle the dataset')
+    parser.add_argument('-s', action='store_true', default=False, help='whether shuffle the dataset')
     parser.add_argument('-warm', type=int, default=1, help='warm up training phase')
     parser.add_argument('-lr', type=float, default=0.1, help='initial learning rate')
     args = parser.parse_args()
