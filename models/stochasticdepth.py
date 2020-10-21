4cc6a35d9dfa54edd636c0fb37f8cfbc385cf61e
weiaicunzai <372995411@qq.com>
2020-10-21 14:33:38 +0800

    refactor: use pytorch buildin bernoulli distribution sampler

--- models/stochasticdepth.py
+++ models/stochasticdepth.py
@@ -18,7 +18,7 @@ class StochasticDepthBasicBlock(torch.jit.ScriptModule):
 
     def __init__(self, p, in_channels, out_channels, stride=1):
         super().__init__()
-        self.p = p
+        self.m = Bernoulli(torch.Tensor([p]))
         self.residual = nn.Sequential(
             nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=stride, padding=1),
             nn.BatchNorm2d(out_channels),
