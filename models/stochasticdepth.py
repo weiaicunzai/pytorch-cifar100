bf2fa9d3e2acd374714188f4c388f4a9a1b29ab0
weiaicunzai <372995411@qq.com>
2020-10-23 21:25:50 +0800

    feat: put tensor p inside a function instead of class member

--- models/stochasticdepth.py
+++ models/stochasticdepth.py
@@ -19,7 +19,8 @@ class StochasticDepthBasicBlock(torch.jit.ScriptModule):
     def __init__(self, p, in_channels, out_channels, stride=1):
         super().__init__()
 
-        self.p = torch.tensor(p).float()
+        #self.p = torch.tensor(p).float()
+        self.p = p
         self.residual = nn.Sequential(
             nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=stride, padding=1),
             nn.BatchNorm2d(out_channels),
