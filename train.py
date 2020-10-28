22c4fae4a89857d981d35dfde293dc9cc794d488
weiaicunzai <372995411@qq.com>
2020-10-28 15:04:05 +0800

    fix: input tensor type and network weights tensor type should be the same
    Details:
    Input type (torch.cuda.FloatTensor) and weight type (torch.FloatTensor) should be the same
    Error occurs, No graph saved
    Traceback (most recent call last):
      File "train.py", line 171, in <module>
        writer.add_graph(net, input_tensor)
      File "/home/by/miniconda3/envs/baiyu_py36/lib/python3.6/site-packages/torch/utils/tensorboard/writer.py", line 714, in add_graph
        self._get_file_writer().add_graph(graph(model, input_to_model, verbose))
      File "/home/by/miniconda3/envs/baiyu_py36/lib/python3.6/site-packages/torch/utils/tensorboard/_pytorch_graph.py", line 291, in graph
        raise e
      File "/home/by/miniconda3/envs/baiyu_py36/lib/python3.6/site-packages/torch/utils/tensorboard/_pytorch_graph.py", line 285, in graph
        trace = torch.jit.trace(model, args)
      File "/home/by/miniconda3/envs/baiyu_py36/lib/python3.6/site-packages/torch/jit/__init__.py", line 955, in trace
        check_tolerance, strict, _force_outplace, _module_class)
      File "/home/by/miniconda3/envs/baiyu_py36/lib/python3.6/site-packages/torch/jit/__init__.py", line 1109, in trace_module
        module._c._create_method_from_trace(method_name, func, example_inputs, var_lookup_fn, strict, _force_outplace)
      File "/home/by/miniconda3/envs/baiyu_py36/lib/python3.6/site-packages/torch/nn/modules/module.py", line 720, in _call_impl
        result = self._slow_forward(*input, **kwargs)
      File "/home/by/miniconda3/envs/baiyu_py36/lib/python3.6/site-packages/torch/nn/modules/module.py", line 704, in _slow_forward
        result = self.forward(*input, **kwargs)
      File "/data/by/pytorch-cifar100/models/vgg.py", line 38, in forward
        output = self.features(x)
      File "/home/by/miniconda3/envs/baiyu_py36/lib/python3.6/site-packages/torch/nn/modules/module.py", line 720, in _call_impl
        result = self._slow_forward(*input, **kwargs)
      File "/home/by/miniconda3/envs/baiyu_py36/lib/python3.6/site-packages/torch/nn/modules/module.py", line 704, in _slow_forward
        result = self.forward(*input, **kwargs)
      File "/home/by/miniconda3/envs/baiyu_py36/lib/python3.6/site-packages/torch/nn/modules/container.py", line 117, in forward
        input = module(input)
      File "/home/by/miniconda3/envs/baiyu_py36/lib/python3.6/site-packages/torch/nn/modules/module.py", line 720, in _call_impl
        result = self._slow_forward(*input, **kwargs)
      File "/home/by/miniconda3/envs/baiyu_py36/lib/python3.6/site-packages/torch/nn/modules/module.py", line 704, in _slow_forward
        result = self.forward(*input, **kwargs)
      File "/home/by/miniconda3/envs/baiyu_py36/lib/python3.6/site-packages/torch/nn/modules/conv.py", line 419, in forward
        return self._conv_forward(input, self.weight)
      File "/home/by/miniconda3/envs/baiyu_py36/lib/python3.6/site-packages/torch/nn/modules/conv.py", line 416, in _conv_forward
        self.padding, self.dilation, self.groups)
    RuntimeError: Input type (torch.cuda.FloatTensor) and weight type (torch.FloatTensor) should be the same

--- train.py
+++ train.py
@@ -167,7 +167,9 @@ if __name__ == '__main__':
     #so the only way is to create a new tensorboard log
     writer = SummaryWriter(log_dir=os.path.join(
             settings.LOG_DIR, args.net, settings.TIME_NOW))
-    input_tensor = torch.Tensor(1, 3, 32, 32).cuda()
+    input_tensor = torch.Tensor(1, 3, 32, 32)
+    if args.gpu:
+        input_tensor = input_tensor.cuda()
     writer.add_graph(net, input_tensor)
 
     #create checkpoint folder to save model
