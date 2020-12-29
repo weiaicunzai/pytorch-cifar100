b9cca0092e0587d09579ddc4bab3f3e2eb540fba
weiaicunzai <372995411@qq.com>
2020-12-29 15:18:04 +0800

    feat: change argument gpu to store_true

--- test.py
+++ test.py
@@ -24,7 +24,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('-net', type=str, required=True, help='net type')
     parser.add_argument('-weights', type=str, required=True, help='the weights file you want to test')
-    parser.add_argument('-gpu', type=bool, default=True, help='use gpu or not')
+    parser.add_argument('-gpu', action='store_true', default=False, help='use gpu or not')
     parser.add_argument('-b', type=int, default=16, help='batch size for dataloader')
     args = parser.parse_args()
 
