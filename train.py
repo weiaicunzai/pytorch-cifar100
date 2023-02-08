426672fe1c75393e1ca5988971d5910da6e36077
Denis Mihailapov <d.mihailapov@expasoft.tech>
2023-02-08 09:23:21 +0700

    add new script's arguments (some on effect),refactoring save/resume model, logging

--- train.py
+++ train.py
@@ -8,21 +8,36 @@ author baiyu
 
 import argparse
 import os
+import random
 import time
+import warnings
+from pathlib import Path
+from pprint import pprint
 
 import torch
 import torch.nn as nn
 import torch.optim as optim
+from torch.backends import cudnn
 from torch.utils.tensorboard import SummaryWriter
 
 from conf import settings
-from utils import get_network, get_training_dataloader, get_test_dataloader, WarmUpLR, \
-    most_recent_folder, most_recent_weights, last_epoch, best_acc_weights
+from utils import WarmUpLR
+from utils import best_acc_weights
+from utils import get_network
+from utils import get_test_dataloader
+from utils import get_training_dataloader
+from utils import last_epoch
+from utils import most_recent_folder
+from utils import most_recent_weights
+from validate_utils import AverageMeter
 
 
-def train(epoch):
+def train(net, epoch):
     start = time.time()
     net.train()
+
+    total_losses = AverageMeter()
+
     for batch_index, (images, labels) in enumerate(cifar100_training_loader):
 
         if args.gpu:
@@ -32,28 +47,32 @@ def train(epoch):
         optimizer.zero_grad()
         outputs = net(images)
         loss = loss_function(outputs, labels)
+
+        total_losses.update(loss.item(), images.size(0))
+
         loss.backward()
         optimizer.step()
 
         n_iter = (epoch - 1) * len(cifar100_training_loader) + batch_index + 1
 
         last_layer = list(net.children())[-1]
-        for name, para in last_layer.named_parameters():
+        for name, param in last_layer.named_parameters():
             if 'weight' in name:
-                writer.add_scalar('LastLayerGradients/grad_norm2_weights', para.grad.norm(), n_iter)
+                writer.add_scalar('LastLayerGradients/grad_norm2_weights', param.grad.norm(), n_iter)
             if 'bias' in name:
-                writer.add_scalar('LastLayerGradients/grad_norm2_bias', para.grad.norm(), n_iter)
+                writer.add_scalar('LastLayerGradients/grad_norm2_bias', param.grad.norm(), n_iter)
 
-        print('Training Epoch: {epoch} [{trained_samples}/{total_samples}]\tLoss: {:0.4f}\tLR: {:0.6f}'.format(
-            loss.item(),
-            optimizer.param_groups[0]['lr'],
-            epoch=epoch,
-            trained_samples=batch_index * args.b + len(images),
-            total_samples=len(cifar100_training_loader.dataset)
-        ))
+        print(
+            f'Training Epoch: {epoch} '
+            f'[{batch_index * args.b + len(images)}/{len(cifar100_training_loader.dataset)}]\t'
+            f'LR: {optimizer.param_groups[0]["lr"]:0.6f}\n'
+        )
 
-        # update training loss for each iteration
-        writer.add_scalar('Train/loss', loss.item(), n_iter)
+        print(
+            f'CE Loss {total_losses.val:.4f} ({total_losses.avg:.4f})\t'
+        )
+
+        writer.add_scalar('Train/loss', total_losses.val, n_iter)
 
         if epoch <= args.warm:
             warmup_scheduler.step()
@@ -61,19 +80,19 @@ def train(epoch):
     for name, param in net.named_parameters():
         layer, attr = os.path.splitext(name)
         attr = attr[1:]
-        writer.add_histogram("{}/{}".format(layer, attr), param, epoch)
+        writer.add_histogram(f'{layer}/{attr}', param, epoch)
 
     finish = time.time()
 
-    print('epoch {} training time consumed: {:.2f}s'.format(epoch, finish - start))
+    print(f'epoch {epoch} training time consumed: {finish - start:.2f}s')
 
 
 @torch.no_grad()
-def eval_training(epoch=0, tb=True):
+def eval_training(net, epoch=0, tb=True):
     start = time.time()
     net.eval()
 
-    test_loss = 0.0  # cost function error
+    test_losses = AverageMeter()
     correct = 0.0
 
     for (images, labels) in cifar100_test_loader:
@@ -85,7 +104,7 @@ def eval_training(epoch=0, tb=True):
         outputs = net(images)
         loss = loss_function(outputs, labels)
 
-        test_loss += loss.item()
+        test_losses.update(loss.item(), images.size(0))
         _, preds = outputs.max(1)
         correct += preds.eq(labels).sum()
 
@@ -94,17 +113,17 @@ def eval_training(epoch=0, tb=True):
         print('GPU INFO.....')
         print(torch.cuda.memory_summary(), end='')
     print('Evaluating Network.....')
-    print('Test set: Epoch: {}, Average loss: {:.4f}, Accuracy: {:.4f}, Time consumed:{:.2f}s'.format(
-        epoch,
-        test_loss / len(cifar100_test_loader.dataset),
-        correct.float() / len(cifar100_test_loader.dataset),
-        finish - start
-    ))
-    print()
+    print(
+        f'Test set: Epoch: {epoch},'
+        f'Average loss: {test_losses.avg:.4f},'
+        f'Accuracy: {correct.float() / len(cifar100_test_loader.dataset):.4f},'
+        f'Time consumed:{finish - start:.2f}s'
+        '\n'
+    )
 
     # add informations to tensorboard
     if tb:
-        writer.add_scalar('Test/Average loss', test_loss / len(cifar100_test_loader.dataset), epoch)
+        writer.add_scalar('Test/Average loss', test_losses.avg, epoch)
         writer.add_scalar('Test/Accuracy', correct.float() / len(cifar100_test_loader.dataset), epoch)
 
     return correct.float() / len(cifar100_test_loader.dataset)
@@ -118,8 +137,84 @@ if __name__ == '__main__':
     parser.add_argument('-b', type=int, default=128, help='batch size for dataloader')
     parser.add_argument('-warm', type=int, default=1, help='warm up training phase')
     parser.add_argument('-lr', type=float, default=0.1, help='initial learning rate')
+    parser.add_argument('-seed', type=int, default=None, help='random seed')
     parser.add_argument('-resume', action='store_true', default=False, help='resume training')
+
+    parser.add_argument('-orig-augs', action='store_true', help='is use orig augs')
+
+    parser.add_argument('-batch-shift', default=1, type=int, help='shift scale for batch augment')
+    parser.add_argument('-use-distil-aug', action='store_true', help='is use distil augmentation loss')
+    parser.add_argument('-distil-aug-weight', default=1.0, type=float, help='cross loss weight')
+    parser.add_argument('-prob-aug', default=1.0, type=float, help='')
+    parser.add_argument('-mode-aug', default="resize", type=str, help='')
+
+    parser.add_argument('-use-cross-loss', action='store_true', help='is use cross samples loss')
+    parser.add_argument('-cross-loss-start-epoch', default=0, type=int,
+                        help='milestone for start to use cross samples loss')
+    parser.add_argument('-cross-loss-weight', default=1.0, type=float, help='cross loss weight')
+
+    parser.add_argument('-use-avg-cross-loss', action='store_true', help='is use cross samples loss')
+    parser.add_argument('-avg-cross-loss-start-epoch', default=0, type=int,
+                        help='milestone for start to use cross samples loss')
+    parser.add_argument('-avg-cross-loss-weight', default=1.0, type=float, help='avg cross loss weight')
+
+    parser.add_argument('-x2-epoch', action='store_true', help='double the number of epochs')
+    parser.add_argument('-bp-filt-size', type=int, default=None, help='')
+
+    parser.add_argument('-teacher', type=str, default='', help='name of folder with model')
+    parser.add_argument('-distil-function', default='l2', type=str, help='distillation function')
+    parser.add_argument('-distil-weight', default=0.0, type=float, help='distillation loss weight')
+    parser.add_argument('-temperature', default=30, type=int, help='soft logits temperature')
+
     args = parser.parse_args()
+
+    if args.x2_epoch:
+        settings.EPOCH *= 2
+        settings.SAVE_EPOCH *= 2
+
+        settings.MILESTONES[0] *= 2
+        settings.MILESTONES[1] *= 2
+        settings.MILESTONES[2] *= 2
+
+        print("New settings")
+        pprint(vars(settings))
+
+    if args.seed is not None:
+        random.seed(args.seed)
+        torch.manual_seed(args.seed)
+        cudnn.deterministic = True
+        warnings.warn('You have chosen to seed training. '
+                      'This will turn on the CUDNN deterministic setting, '
+                      'which can slow down your training considerably! '
+                      'You may see unexpected behavior when restarting '
+                      'from checkpoints.')
+
+    exp_name = args.net
+
+    if args.bp_filt_size:
+        exp_name += f"_lpf{args.bp_filt_size}"
+
+    exp_name += f"_bss{args.batch_shift}"
+
+    if args.orig_augs:
+        exp_name += "_orig_augs"
+    else:
+        exp_name += f"_rc_aug_{args.prob_aug}_aug_mode_{args.mode_aug}"
+
+    if args.use_cross_loss:
+        exp_name += f"_log_cross_loss_{args.cross_loss_weight}w"
+        if args.cross_loss_start_epoch > 0:
+            exp_name += f"_start{args.cross_loss_start_epoch}"
+
+    if args.use_avg_cross_loss:
+        exp_name += f"_log_avg_cross_loss_{args.avg_cross_loss_weight}w"
+        if args.avg_cross_loss_start_epoch > 0:
+            exp_name += f"_start{args.avg_cross_loss_start_epoch}"
+
+    if args.x2_epoch:
+        exp_name += "_x2_epoch"
+
+    exp_name += "_log_no_w"
 
     net = get_network(args)
 
@@ -147,15 +242,26 @@ if __name__ == '__main__':
     iter_per_epoch = len(cifar100_training_loader)
     warmup_scheduler = WarmUpLR(optimizer, iter_per_epoch * args.warm)
 
-    if args.resume:
-        recent_folder = most_recent_folder(os.path.join(settings.CHECKPOINT_PATH, args.net), fmt=settings.DATE_FORMAT)
-        if not recent_folder:
-            raise Exception('no recent folder were found')
+    checkpoint_path = Path(settings.CHECKPOINT_PATH)
 
-        checkpoint_path = os.path.join(settings.CHECKPOINT_PATH, args.net, recent_folder)
+    if args.use_distil_aug:
+        checkpoint_path = checkpoint_path / 'distil_aug'
+        checkpoint_path = checkpoint_path / f'w_{args.distil_aug_weight}' \
+                                            f'_func_{args.distil_function}' \
+                                            f'_temp_{args.temperature}_1mlstone'
+
+    checkpoint_path = checkpoint_path / exp_name / f"seed{args.seed}"
+
+    if args.resume:
+        recent_folder = most_recent_folder(
+            str(checkpoint_path),
+            fmt=settings.DATE_FORMAT
+        )
+
+        checkpoint_path = checkpoint_path / recent_folder
 
     else:
-        checkpoint_path = os.path.join(settings.CHECKPOINT_PATH, args.net, settings.TIME_NOW)
+        checkpoint_path = checkpoint_path / settings.TIME_NOW
 
     # use tensorboard
     if not os.path.exists(settings.LOG_DIR):
@@ -163,37 +269,45 @@ if __name__ == '__main__':
 
     # since tensorboard can't overwrite old values
     # so the only way is to create a new tensorboard log
+    log_dir = settings.LOG_DIR
+    if args.use_distil_aug:
+        log_dir = os.path.join(
+            log_dir, 'distil_aug',
+            f'w_{args.distil_aug_weight}_'
+            f'func_{args.distil_function}_temp_{args.temperature}_1mlstone'
+        )
+
     writer = SummaryWriter(log_dir=os.path.join(
-        settings.LOG_DIR, args.net, settings.TIME_NOW))
+        log_dir, exp_name, f"seed{args.seed}", settings.TIME_NOW))
+
     input_tensor = torch.Tensor(1, 3, 32, 32)
     if args.gpu:
         input_tensor = input_tensor.cuda()
     writer.add_graph(net, input_tensor)
 
     # create checkpoint folder to save model
-    if not os.path.exists(checkpoint_path):
-        os.makedirs(checkpoint_path)
-    checkpoint_path = os.path.join(checkpoint_path, '{net}-{epoch}-{type}.pth')
+    checkpoint_path.mkdir(exist_ok=True, parents=True)
 
     best_acc = 0.0
+    resume_epoch = -1
     if args.resume:
-        best_weights = best_acc_weights(os.path.join(settings.CHECKPOINT_PATH, args.net, recent_folder))
+        best_weights = best_acc_weights(str(checkpoint_path))
         if best_weights:
-            weights_path = os.path.join(settings.CHECKPOINT_PATH, args.net, recent_folder, best_weights)
-            print('found best acc weights file:{}'.format(weights_path))
+            weights_path = checkpoint_path / best_weights
+            print(f'found best acc weights file: {weights_path}')
             print('load best training file to test acc...')
             net.load_state_dict(torch.load(weights_path))
-            best_acc = eval_training(tb=False)
-            print('best acc is {:0.2f}'.format(best_acc))
+            best_acc = eval_training(net, tb=False)
+            print(f'best acc is {best_acc:0.2f}')
 
-        recent_weights_file = most_recent_weights(os.path.join(settings.CHECKPOINT_PATH, args.net, recent_folder))
+        recent_weights_file = most_recent_weights(str(checkpoint_path))
         if not recent_weights_file:
             raise Exception('no recent weights file were found')
-        weights_path = os.path.join(settings.CHECKPOINT_PATH, args.net, recent_folder, recent_weights_file)
-        print('loading weights file {} to resume training.....'.format(weights_path))
+        weights_path = checkpoint_path / recent_weights_file
+        print(f'loading weights file {weights_path} to resume training.....')
         net.load_state_dict(torch.load(weights_path))
 
-        resume_epoch = last_epoch(os.path.join(settings.CHECKPOINT_PATH, args.net, recent_folder))
+        resume_epoch = last_epoch(str(checkpoint_path))
 
     for epoch in range(1, settings.EPOCH + 1):
         if epoch > args.warm:
@@ -203,20 +317,20 @@ if __name__ == '__main__':
             if epoch <= resume_epoch:
                 continue
 
-        train(epoch)
-        acc = eval_training(epoch)
+        train(net, epoch)
+        acc = eval_training(net, epoch)
 
         # start to save best performance model after learning rate decay to 0.01
         if epoch > settings.MILESTONES[1] and best_acc < acc:
-            weights_path = checkpoint_path.format(net=args.net, epoch=epoch, type='best')
-            print('saving weights file to {}'.format(weights_path))
+            weights_path = checkpoint_path / f'{args.net}-{epoch}-best.pth'
+            print(f'saving weights file to {weights_path}')
             torch.save(net.state_dict(), weights_path)
             best_acc = acc
             continue
 
         if not epoch % settings.SAVE_EPOCH:
-            weights_path = checkpoint_path.format(net=args.net, epoch=epoch, type='regular')
-            print('saving weights file to {}'.format(weights_path))
+            weights_path = checkpoint_path / f'{args.net}-{epoch}-regular.pth'
+            print(f'saving weights file to {weights_path}')
             torch.save(net.state_dict(), weights_path)
 
     writer.close()
