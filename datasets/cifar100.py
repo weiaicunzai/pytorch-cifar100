08e2185bf7279a37d78fb43476801ccb333eae94
Denis Mihailapov <d.mihailapov@expasoft.tech>
2023-03-13 12:09:28 +0700

    add cifar100 test dataloader for validation

--- datasets/cifar100.py
+++ datasets/cifar100.py
@@ -6,10 +6,26 @@ author baiyu
 from torchvision.datasets import CIFAR100
 
 from datasets.utils import construct_dataloaders
+from datasets.utils import get_test_dataloader
 
 # mean and std of cifar100 dataset
 _CIFAR100_TRAIN_MEAN = (0.5070751592371323, 0.48654887331495095, 0.4409178433670343)
 _CIFAR100_TRAIN_STD = (0.2673342858792401, 0.2564384629170883, 0.27615047132568404)
+
+
+def get_cifar100_test_dataloader(batch_size=128, shuffle=False, num_workers=4, img_pad=0):
+    test_loader = get_test_dataloader(
+        CIFAR100, root='./data/',
+        mean=_CIFAR100_TRAIN_MEAN,
+        std=_CIFAR100_TRAIN_STD,
+        img_size=32,
+        num_workers=num_workers,
+        batch_size=batch_size,
+        shuffle=shuffle,
+        img_pad=img_pad
+    )
+
+    return test_loader
 
 
 def get_cifar100_dataloaders(args):
